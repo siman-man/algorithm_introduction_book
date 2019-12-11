a172d981cf79e68abf7a895d6d6182244e2d159f
Shuichi Tamayose <tmshuichi@gmail.com>
2019-12-11 11:24:14 +0900

    Update check_step_count.rb

--- chapter01/practices/check_step_count.rb
+++ chapter01/practices/check_step_count.rb
@@ -1,4 +1,11 @@
+# 1.2-2
 insert_sort_step = -> (n) { 8 * n ** 2 }
 merge_sort_step = -> (n) { 64 * n * Math.log2(n) }
 
 p (2..).take_while { |n| insert_sort_step[n] < merge_sort_step[n] }
+
+# 1.2-3
+algo_a = -> (n) { 100 * n ** 2 }
+algo_b = -> (n) { 2 ** n }
+
+p (1..).find { |n| algo_a[n] < algo_b[n] }
